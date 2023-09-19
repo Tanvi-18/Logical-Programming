f348fe54ca9ae80523c464790c2cac659084cb6e
Tanvi-18 <tanvi18parab@gmail.com>
2023-09-19 22:54:35 +0530

    Number based programs in Java

---
+++ CoPrimeNumbers.java
@@ -0,0 +1,35 @@
+package com.jsp.LogicalProgramming;
+
+import java.util.Scanner;
+
+public class CoPrimeNo {
+	
+	public static int greatestCommonDivisor(int num1, int num2) {
+		int gcd = 1;
+
+		for (int i = 1; i <= num1 && i <= num2; i++) {
+
+			if (num1 % i == 0 && num2 % i == 0) {
+				gcd = i;
+			}
+		}
+		return gcd;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first number");
+		int a = sc.nextInt();
+		System.out.println("Enter second number");
+		int b = sc.nextInt();
+
+		if (greatestCommonDivisor(a, b) == 1) {
+			System.out.println(a + " & " + b + " are Co Prime Numbers");
+		} else {
+			System.out.println(a + " & " + b + " not Co Prime Numbers");
+		}
+
+	}
+
+}
